9fdac60e7b0339cbcb63d16ddb6534823d244b17
mahmoud rabea <mahmoudrabea082@gmail.com>
2021-05-13 02:23:02 +0200

    adding icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,16 +5,42 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        flexibleSpace: Container(
-          height: MediaQuery.of(context).size.height*0.1,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("Assets/Icons/logo.png"),
+        actions: [
+          Builder(builder: (context) {
+            return GestureDetector(
+                onTap: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+                child: Image.asset("Assets/Icons/menu.png"));
+          }),
+        ],
+        leading: GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: MediaQuery.of(context).size.height * .06,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("Assets/Icons/search.png"),
+              ),
             ),
           ),
         ),
+        backgroundColor: Theme.of(context).primaryColor,
+        flexibleSpace: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * .06,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("Assets/Icons/logo.png"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+      endDrawer: Drawer(),
     );
   }
 }
