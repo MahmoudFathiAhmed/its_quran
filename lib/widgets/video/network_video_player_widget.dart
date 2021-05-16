22a1529bf56e96cae76860c770e540c884cb7837
mahmoud rabea <mahmoudrabea082@gmail.com>
2021-05-16 23:30:35 +0200

    adding play videoss

--- lib/widgets/video/network_video_player_widget.dart
+++ lib/widgets/video/network_video_player_widget.dart
@@ -20,10 +20,15 @@ class _NetworkVideoPlayerWidgetState extends State<NetworkVideoPlayerWidget> {
   void initState() {
     super.initState();
 
-    controller = VideoPlayerController.network('https://youtu.be/N1ZIp1Rbq28')
+    controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
       ..addListener(() => setState(() {}))
       ..setLooping(true)
-      ..initialize().then((_) => controller.play());
+      ..initialize().then((_) {
+        setState(() {
+          controller.play();
+        });
+      });
   }
 
   @override
@@ -33,12 +38,68 @@ class _NetworkVideoPlayerWidgetState extends State<NetworkVideoPlayerWidget> {
   }
 
   @override
-  Widget build(BuildContext context) => Container(
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            VideoPlayerWidget(controller: controller),
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          VideoPlayerWidget(controller: controller),
+        ],
+      ),
+    );
+  }
+
+  Widget videoplayerwidget(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      width: MediaQuery.of(context).size.width,
+      child: AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            VideoPlayer(controller),
+            ClosedCaption(text: controller.value.caption.text),
+            _PlayPauseOverlay(controller: controller),
+            VideoProgressIndicator(controller, allowScrubbing: true),
           ],
         ),
-      );
+      ),
+    );
+  }
+}
+
+class _PlayPauseOverlay extends StatelessWidget {
+  const _PlayPauseOverlay({Key key, this.controller}) : super(key: key);
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        AnimatedSwitcher(
+          duration: Duration(milliseconds: 50),
+          reverseDuration: Duration(milliseconds: 200),
+          child: controller.value.isPlaying
+              ? SizedBox.shrink()
+              : Container(
+                  color: Colors.black26,
+                  child: Center(
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 60.0,
+                    ),
+                  ),
+                ),
+        ),
+        GestureDetector(
+          onTap: () {
+            controller.value.isPlaying ? controller.pause() : controller.play();
+          },
+        ),
+      ],
+    );
+  }
 }
